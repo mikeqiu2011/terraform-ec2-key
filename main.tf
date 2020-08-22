998845e26846a4fc361830a1c0cc83a7a85e2148
mikeqiu2011 <mikeqiu2011@163.com>
2020-08-22 20:32:43 +0800

    install public key

--- main.tf
+++ main.tf
@@ -11,7 +11,28 @@ provider "aws" {
   region  = "us-east-2"
 }
 
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("~/.ssh/terraform.pub")
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-058043b9f3c10c078"
+  key_name      = aws_key_pair.example.key_name
+  ami           = "ami-07c8bc5c1ce9598c3"
   instance_type = "t2.micro"
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/terraform")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable nginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx"
+    ]
+  }
 }
